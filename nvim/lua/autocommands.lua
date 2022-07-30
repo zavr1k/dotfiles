2ac481aa8dbbf9f86979b2a74e28a1e515b42550
Anton <s.anton@live.ru>
2022-07-30 13:43:10 +0300

    Added nvim autocommands

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,6 +1,18 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 local opt = vim.opt
+local opt_local = vim.opt_local
+
 -- Отключает автокомментирование новой строки через о О
-vim.api.nvim_create_autocmd({'BufEnter'}, {
+-- vim.api.nvim_create_autocmd({'BufEnter'}, {
+--   pattern = '*',
+--   callback = function()
+--     opt.fo:remove('c')
+--     opt.fo:remove('r')
+--     opt.fo:remove('o')
+--   end
+-- })
+autocmd({'BufEnter'}, {
   pattern = '*',
   callback = function()
     opt.fo:remove('c')
@@ -10,8 +22,19 @@ vim.api.nvim_create_autocmd({'BufEnter'}, {
 })
 
 -- Подсветка скопированных строк
-local YankHighlightGrp = vim.api.nvim_create_augroup('YankHighlightGrp', {})
-vim.api.nvim_create_autocmd('TextYankPost', {
+-- local YankHighlightGrp = vim.api.nvim_create_augroup('YankHighlightGrp', {})
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+-- 	group = YankHighlightGrp,
+--   pattern = '*',
+--   callback = function()
+--     vim.highlight.on_yank({
+--       higroup = 'IncSearch',
+--       timeout = 100,
+--     })
+--   end,
+-- })
+local YankHighlightGrp = augroup('YankHighlightGrp', {})
+autocmd('TextYankPost', {
 	group = YankHighlightGrp,
   pattern = '*',
   callback = function()
@@ -22,3 +45,25 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- 2 spaces for selected filetypes
+autocmd({'FileType'}, {
+  pattern = 'xml,html,xhtml,css,scss,javascript,lua,yaml,htmljinja',
+  callback = function()
+    opt_local.shiftwidth = 2
+    opt_local.tabstop = 2
+  end
+})
+
+--- remove all trailing whitespace on save
+local TrimWhiteSpaceGrp = augroup('TrimWhiteSpaceGrp', {})
+autocmd('BufWritePre', {
+	group = TrimWhiteSpaceGrp,
+  pattern = '*',
+  command = '%s/\\s\\+$//e',
+})
+
+-- sql: format sql files on save via pgFormatter
+-- autocmd({'BufWritePre'}, {
+--   pattern = '*.sql',
+--   command = ':%!pg_format'
+-- })
