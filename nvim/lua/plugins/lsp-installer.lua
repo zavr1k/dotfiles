fda4aad6cb30aa1fdc78b9f05e8c622c938e3d8c
Anton <s.anton@live.ru>
2022-07-30 20:03:58 +0300

    Added autocomplete with cmp

--- nvim/lua/plugins/lsp-installer.lua
+++ nvim/lua/plugins/lsp-installer.lua
@@ -1,14 +1,60 @@
 local lsp_installer = require("nvim-lsp-installer")
+local map = vim.keymap.set
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+ -- Diagnostic keymaps
+map('n', '<leader>e', vim.diagnostic.open_float)
+map('n', '[d', vim.diagnostic.goto_prev)
+map('n', ']d', vim.diagnostic.goto_next)
+map('n', '<leader>l', vim.diagnostic.setloclist)
+
+-- LSP settings
+local on_attach = function(_, bufnr)
+  local opts = { buffer = bufnr }
+  map('n', 'gD', vim.lsp.buf.declaration, opts)
+  -- map('n', 'gd', vim.lsp.buf.definition, opts)
+  map('n', 'K', vim.lsp.buf.hover, opts)
+  map('n', 'gi', vim.lsp.buf.implementation, opts)
+  map('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+  map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+  map('n', '<leader>wl', function()
+    vim.inspect(vim.lsp.buf.list_workspace_folders())
+  end, opts)
+  map('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+  map('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  map('n', '<leader>gr', vim.lsp.buf.references, opts)
+  map('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+  map('n', '<leader>so', require('telescope.builtin').lsp_document_symbols, opts)
+  vim.api.nvim_create_user_command("Format", vim.lsp.buf.formatting, {})
+end
+
+local cmp = require('cmp')
+cmp.setup{
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'path' },
+    { name = 'buffer', option = {
+      get_bufnrs = function()
+        return vim.api.nvim_list_bufs()
+      end
+      },
+    },
+  },
+}
+
 lsp_installer.on_server_ready(function(server)
   local opts = {
-    on_attach = function() 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = 0})
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-      vim.keymap.set('n', 'gD', vim.lsp.buf.definition, {buffer = 0})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {buffer = 0})
-      vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {buffer = 0})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {buffer = 0})
-    end,
+    on_attach = on_attach,
+    capabilities = capabilities
   }
   server:setup(opts)
 end)
